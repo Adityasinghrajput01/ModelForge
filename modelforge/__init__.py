7f76eb0f4cec0526c602fdc02ca30d6c5a9a7959
Adityasinghrajput01 <adityasingh45245@gmail.com>
2026-09-26 17:10:13 +0530

    Add public API and package configuration

---
+++ modelforge/__init__.py
@@ -0,0 +1,16 @@
+"""
+ModelForge
+==========
+
+A transparent, local-first AutoML framework.
+"""
+
+from modelforge.automl import AutoML, automl
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "AutoML",
+    "__version__",
+    "automl",
+]
